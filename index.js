50211c12e9ff9ae8dcf38d72bf96ce41883e0f2c
Christine QE <christine.q.ernst@gmail.com>
2017-10-25 00:45:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,8 +22,10 @@ function currentLine(katzDeliLine) {
   if (katzDeliLine[0]) {
     for (var i = 0; i<katzDeliLine.length; i++) {
     var position = i+1 + ".";
-    currentLineStatement.push(" " + position + " " + katzDeliLine[i] + ",");
+    currentLineStatement.push(" " + position + " " + katzDeliLine[i]);
+    currentLineStatement.push(",");
     }
+  currentLineStatement.pop();
   return "".concat(...currentLineStatement);
   } else {
   return "The line is currently empty."
